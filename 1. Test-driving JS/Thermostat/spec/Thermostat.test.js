2a3f420b89c87e4b5da31329ff84d0515471ff32
nickwlong <nickwlong@gmail.com>
2022-09-12 12:57:07 +0100

    html reporter?

--- 1. Test-driving JS/Thermostat/spec/Thermostat.test.js
+++ 1. Test-driving JS/Thermostat/spec/Thermostat.test.js
@@ -6,24 +6,24 @@ describe("Thermostat", function () {
   });
 
   describe("getTemperature", function () {
-    it("Returns the default temperature", function () {
+    test("Returns the default temperature", function () {
       expect(thermo.getTemperature()).toBe(20);
     });
   });
 
   describe("up and down", function () {
-    it("Increases the temperature by one degree", function () {
+    test("Increases the temperature by one degree", function () {
       thermo.up();
       expect(thermo.getTemperature()).toBe(21);
     });
 
-    it("Increases the temperature by one degree after running up twice", function () {
+    test("Increases the temperature by one degree after running up twice", function () {
       thermo.up();
       thermo.up();
       expect(thermo.getTemperature()).toBe(22);
     });
 
-    it("Increases temp by 2 then down by 1", function () {
+    test("Increases temp by 2 then down by 1", function () {
       thermo.up();
       thermo.up();
       thermo.down();
@@ -32,13 +32,13 @@ describe("Thermostat", function () {
   });
 
   describe("#setPowerSavingMode", function () {
-    it("Limits maximum temperature to 25 degrees when on", function () {
+    test("Limits maximum temperature to 25 degrees when on", function () {
       for (let i = 0; i < 10; i++) {
         thermo.up();
       }
       expect(thermo.getTemperature()).toBe(25);
     });
-    it("Limits maximum temperature to 32 degrees when off", function () {
+    test("Limits maximum temperature to 32 degrees when off", function () {
       thermo.setPowerSavingMode(false);
       for (let i = 0; i < 20; i++) {
         thermo.up();
@@ -48,7 +48,7 @@ describe("Thermostat", function () {
   });
 
   describe("#reset", function () {
-    it("resets the temperature to the default", function () {
+    test("resets the temperature to the default", function () {
       thermo.setPowerSavingMode(false);
       for (let i = 0; i < 20; i++) {
         thermo.up();
@@ -59,21 +59,21 @@ describe("Thermostat", function () {
   });
 
   describe("#energyUse", function () {
-    it("describes low energy use for < 18", function () {
+    test("describes low energy use for < 18", function () {
       for (let i = 0; i < 4; i++) {
         thermo.down();
       }
       expect(thermo.getTemperature()).toBe(16);
       expect(thermo.energyUse()).toBe("low-usage");
     });
-    it("describes medium energy use for >= 18, <=25", function () {
+    test("describes medium energy use for >= 18, <=25", function () {
       for (let i = 0; i < 5; i++) {
         thermo.up();
       }
       expect(thermo.getTemperature()).toBe(25);
       expect(thermo.energyUse()).toBe("medium-usage");
     });
-    it("describes high energy use for > 25", function () {
+    test("describes high energy use for > 25", function () {
       thermo.setPowerSavingMode(false);
 
       for (let i = 0; i < 6; i++) {
