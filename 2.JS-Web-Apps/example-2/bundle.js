90fbbba013910bae4bc0c929f21d731b6273ac97
nickwlong <nickwlong@gmail.com>
2022-09-13 19:42:53 +0100

    Test driven dynamic javascript

--- 2.JS-Web-Apps/example-2/bundle.js
+++ 2.JS-Web-Apps/example-2/bundle.js
@@ -17,6 +17,17 @@
           newParagraph.innerText = "I was created dynamically";
           this.mainContainerEl.append(newParagraph);
         }
+        displayTitle() {
+          const newTitle = document.createElement("h2");
+          newTitle.innerText = "My amazing website";
+          this.mainContainerEl.append(newTitle);
+        }
+        deleteParagraphs() {
+          const paragraphs = document.querySelectorAll("p");
+          paragraphs.forEach((paragraph) => {
+            paragraph.remove();
+          });
+        }
       };
       module.exports = View2;
     }
--- 2.JS-Web-Apps/example-2/view.test.js
+++ 2.JS-Web-Apps/example-2/view.test.js
@@ -6,13 +6,42 @@
  const View = require('./view');
  
  describe('Page view', () => {
-   it('displays 2 paragraphs', () => {
+
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+  });
+
+
+  it('displays 2 paragraphs', () => {
      document.body.innerHTML = fs.readFileSync('./index.html');
  
      const view = new View();
  
      expect(document.querySelectorAll('p').length).toBe(2);
-   });
+  });
+
+  it('displays 4 paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new View();
+    view.addParagraph();
+    view.addParagraph();
+
+    expect(document.querySelectorAll('p').length).toBe(4);
+ });
+
+  it('displays a title', () => {
+    const view = new View();
+    view.displayTitle();
+    expect(document.querySelector('h2').innerText).toBe('My amazing website');
+  });
+
+  it('deletes all paragraphs', () => {
+    const view = new View();
+    view.deleteParagraphs();
+    expect(document.querySelectorAll('p').length).toBe(0);
+  })
+
  });
 
  // npm i jest-environment-jsdom
