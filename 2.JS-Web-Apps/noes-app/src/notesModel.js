0e52bdd92df9235ee5e936eb76ca4766272402d5
nickwlong <nickwlong@gmail.com>
2022-09-13 14:55:05 +0100

    Test driven some notes functionality

---
+++ 2.JS-Web-Apps/noes-app/src/notesModel.js
@@ -0,0 +1,16 @@
+class NotesModel {
+  constructor() {
+    this.notes = []
+  }
+  getNotes(){
+    return this.notes
+  }
+  addNote(note){
+    this.notes.push(note)
+  }
+  reset() {
+    this.notes = []
+  }
+}
+
+module.exports = NotesModel
