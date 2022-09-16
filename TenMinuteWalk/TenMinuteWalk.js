bd29c90b2b561ab6a09520517fccf8edfb5094c3
nickwlong <nickwlong@gmail.com>
2022-09-16 09:24:38 +0100

    Ten Minute Walk refactoring

--- TenMinuteWalk/TenMinuteWalk.js
+++ TenMinuteWalk/TenMinuteWalk.js
@@ -6,39 +6,36 @@ class TenMinuteWalk {
   verticalCheck() {
     let verticalCount = 0
     this.array.forEach(direction => {
-      if(direction === "n") {verticalCount += 1} else if 
-      (direction === "s") {verticalCount -= 1}
+      if(direction === "n") {
+        verticalCount += 1
+      } else if (direction === "s") {
+        verticalCount -= 1}
     });
-    return this.isCheckZero(verticalCount)
+    return (verticalCount === 0)
   }
 
   horizontalCheck() {
     let horizontalCount = 0
     this.array.forEach(direction => {
-      if(direction === "w") {horizontalCount += 1} else if 
-      (direction === "e") {horizontalCount -= 1}
+      if(direction === "w") {
+        horizontalCount += 1
+      } else if (direction === "e") {
+        horizontalCount -= 1}
     });
-    return this.isCheckZero(horizontalCount)
-  }
-
-  isCheckZero(count) {
-    if (count == 0 ) {return true} else {return false}
+    return (horizontalCount === 0)
   }
 
   isTenMinutes() {
-    if (this.array.length === 10) {return true} 
-    else {return false};
+    return (this.array.length === 10);
   }
 
   isZeroDisplacement() {
-    if(this.verticalCheck() === false || this.horizontalCheck() === false) {return false} else {return true}
+    return(this.verticalCheck() === true && this.horizontalCheck() === true)
   }
 
   isTenMinuteWalk() {
-    if(this.isZeroDisplacement() && this.isTenMinutes()) {return true} else {return false}
-
+    return(this.isZeroDisplacement() && this.isTenMinutes())
   }
-
 }
 
 module.exports = TenMinuteWalk
